c394c48d417e8b3edf41b3a89e0b210adf658559
Jean Marc Dossou <jeanmarcdossou77@gmail.com>
2026-04-16 06:40:07 +0100

    Fix route attestation + suppression doublon

--- app/Http/Controllers/AttestationController.php
+++ app/Http/Controllers/AttestationController.php
@@ -42,9 +42,26 @@ class AttestationController extends Controller
         return $pdf->download($filename);
     }
 
-            public function telecharger($id)
+public function telecharger($id)
 {
-    return "Téléchargement attestation ID : " . $id;
+    $user = Auth::user();
+
+    $pathologiesPubliees = Remede::where('user_id', $user->id)
+        ->where('approuve', true)
+        ->with('pathologie')
+        ->get()
+        ->pluck('pathologie.nom')
+        ->filter()
+        ->unique()
+        ->values()
+        ->toArray();
+
+    $pdf = Pdf::loadView('attestation.pdf', compact('user', 'pathologiesPubliees'))
+        ->setPaper('a4', 'portrait');
+
+    $filename = 'Attestation_BioLink_' . str_replace(' ', '_', $user->name) . '_' . now()->format('Y') . '.pdf';
+
+    return $pdf->download($filename);
 }
 
 }
